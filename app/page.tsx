39687cf3ec4f8ef960663e02cdab1d3252dba461
James Yang <77857413+jamesyang02@users.noreply.github.com>
2024-09-27 18:29:31 -0700

    Cloned from the backend repo

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,4 @@ import { redirect } from "next/navigation";
 
 export default async function Page() {
   redirect('/home');
-  return (
-    // This is the home page wrapper
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    </div>
-  );
 }
