d8f0091b1104b0ea98a00ffdedf14c0057f4bf76
James Yang <77857413+jamesyang02@users.noreply.github.com>
2024-09-29 14:43:37 -0700

    adding Groq recipe recommendations as an extra feature in the results page

--- app/home/results/page.tsx
+++ app/home/results/page.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from 'next/dynamic';
 import { useState, useEffect } from 'react';
-import Image from 'next/image';
+import axios from 'axios';
 
 import { Dela_Gothic_One } from 'next/font/google';
 const dela = Dela_Gothic_One({ weight: ['400'], subsets: ['latin'] })
@@ -13,7 +13,7 @@ export default function Page() {
   });
 
   const [data, setData] = useState<any>([]);
-  const [imageURL, setImageURL] = useState<string>();
+  const [groqData, setGroqData] = useState<any>([]);
 
   useEffect(() => {
     async function fetchData() {
@@ -22,8 +22,32 @@ export default function Page() {
         setData(JSON.parse(foodJsonString));
       }
     }
+
+    async function fetchGroqRecs() {
+      // use axios to query groq
+      // create a form with foodJsonString
+      const form = new FormData();
+      var foodNames = null;
+      const foodJsonString = sessionStorage.getItem("foodDisplayList");
+      if (foodJsonString) {
+        foodNames = JSON.parse(foodJsonString).map((object: any) => object.item.item.Name);
+      }
+      if (foodNames) {
+        form.append("itemsList", JSON.stringify(foodNames));
+        console.log("foodNames: " + JSON.stringify(foodNames));
+      }
+      // send the form to the groq endpoint
+      const response = await axios.post("/api/groq", form);
+      if (response.data) {
+        setGroqData(response.data);
+      }
+    }
+
     fetchData();
+    fetchGroqRecs();
   }, []);
+
+  
 
   return (
     <div id="resultsWrapper" className="mx-auto w-9/12">
@@ -40,6 +64,20 @@ export default function Page() {
             ))}
         </div>
       </div>
+      <div className="relative mx-auto w-full text-center">
+        <h1 className={dela.className + " p-10 text-4xl font-bold"}>Recipe Recommendation</h1>
+      </div>
+      <div className="flex md:flex-row flex-col flex-auto">
+        <div id="groqItemsList" className="md:w-10/12 w-full">
+          {groqData.map((object: any) => (
+            <DynamicListItem 
+              key={object.key}
+              item={object.item}
+            />
+            ))}
+        </div>
+      </div>
+      <div id="footing" className="h-44"></div>
     </div>
   );
 }
